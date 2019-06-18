528c5e562eb87216faea6bc7bfdf21ac7f652b26
denis <dlcastro@artit.com.br>
2019-06-18 15:03:04 -0300

    B7  Loops

--- js/script.js
+++ js/script.js
@@ -7,13 +7,65 @@ function trocarDiv(nome, idade) {
     area.innerHTML = nome+" "+texto+" tem "+idade+" anos";
 }
 */
+//**************************************************************************************************************************************** */
+/*
+            ---------------------------
+            --JS DA LISTA DE ARQUIVOS--
+            ---------------------------
 
 function adicionarIngrediente() {
-    var ing = document.getElementeById("ingrediente").value;
+    var ing = document.getElementById("ingrediente").value;
 
     var listahtml = document.getElementById("lista").innerHTML;
 
     listahtml = listahtml + "<li>"+ing+"</li>";
 
     document.getElementById("lista").innerHTML = listahtml;
+}
+*/
+//**************************************************************************************************************************************** */
+/*
+var x = 10;
+var y = x + 30;
+
+alert(y);
+*/
+//**************************************************************************************************************************************** */
+/*
+                -----------------------
+                --OPERAÇÃO MATEMATICA--
+                -----------------------
+
+function multiplicar() {
+    var campo1 = parseInt(document.getElementById("campo1").value);
+    var campo2 = parseInt(document.getElementById("campo2").value);
+
+    var multiplicacao = campo1 * campo2;
+
+    alert(multiplicacao);
+}
+
+*/
+//**************************************************************************************************************************************** */
+/*
+                    ------------------------------
+                    --EXEMPLO DE VERIFICADOR(IF)--
+                    ------------------------------
+
+function testeVerificador() {
+    var x = prompt("Diga um número: ");
+
+    if(x > 10) {
+        alert("É maior que 10");
+    } else {
+        alert("Não é maior que 10");
+    }
+}
+*/
+
+
+var x = 0;
+while(x = 10) {
+    document.write("Numero: "+x+"<br/>")
+    x++;
 }
