2ed9333b12883fc94ffc7db90d7f7350ad82f5a5
denis <dlcastro@artit.com.br>
2019-06-24 15:00:32 -0300

    INICIO CURSO JSINTERMEDIARIO AULA EVENTOS DE MOUSE

---
+++ jsIntermediario/script.js
@@ -0,0 +1,25 @@
+/*
+function apertouMouse() {
+    console.log("APERTOU O MOUSE");
+}
+function soltouMouse() {
+    console.log("SOLTOU O MOUSE");
+}
+function mouseEmCima() {
+    console.log("MOUSE PASSOU EM CIMA");
+}   
+function mouseSaiuDeCima() {
+    console.log("MOUSE SAIU DE CIMA");
+}
+function moveuMouse() {
+    console.log("MOUSE FOI MOVIDO");
+}
+*/
+
+function clicou() {
+	console.log("CLICOU");
+}
+
+function botaoDireito() {
+	console.log("BOTAO DIREITO");
+}
