40be762c1ba398d6776cd98aa5eff1c39b079ec9
denis <dlcastro@artit.com.br>
2019-06-25 14:48:46 -0300

    Aula Eventos de Formulários

--- jsIntermediario/script.js
+++ jsIntermediario/script.js
@@ -1,4 +1,7 @@
 /*
+        ---------------------
+        --FUNCTION DE MOUSE--
+        ---------------------
 function apertouMouse() {
     console.log("APERTOU O MOUSE");
 }
@@ -16,10 +19,52 @@ function moveuMouse() {
 }
 */
 
+/*
+        ------------------------------
+        --FUNCTION DE CLICK DO MOUSE--
+        ------------------------------
 function clicou() {
 	console.log("CLICOU");
 }
+*/
 
+/*
 function botaoDireito() {
-	console.log("BOTAO DIREITO");
+    console.log("BOTAO DIREITO");
+    
+    return false;
+}
+*/
+
+/*
+function clickDuplo() {
+    console.log("CLICOU DUAS VEZES");
+}
+*/
+
+/*
+        -----------------------
+        --FUNCTION DE TECLADO--
+        -----------------------
+function apertouTecla(event) {
+    console.log("APERTOU ALGUMA TECLA: "+event.keycode);
+}
+*/
+
+/*
+function apertouTecla(event) {
+    if(event.shiftKey == true && event.keyCode == 69) {
+        console.log("Apertou Shift + E");
+    }
+}
+*/
+
+/*
+function carregou() {
+    alert("Página carregada");
+}
+*/
+
+function mudouOpcao(objeto) {
+    console.log("Selecionou: "+objeto.value);
 }
