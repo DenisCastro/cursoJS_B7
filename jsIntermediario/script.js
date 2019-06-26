5ea1a3f28b8f3114d41d948c3c2b520b47ac898f
denis <dlcastro@artit.com.br>
2019-06-26 14:53:54 -0300

    Projeto:Validação de Formulários

--- jsIntermediario/script.js
+++ jsIntermediario/script.js
@@ -65,6 +65,43 @@ function carregou() {
 }
 */
 
+/*
+        --------------
+        --FORMULARIO--
+        --------------
+
 function mudouOpcao(objeto) {
     console.log("Selecionou: "+objeto.value);
 }
+function focou() {
+    console.log("Focou no campo 1");
+}
+function desfocou() {
+    console.log("Desfocou do campo 1");
+}
+*/
+/*
+        ----------------
+        --FUNCAO BOLAS--
+        ----------------
+
+function addBola() {
+    var bola = document.createElement("div");
+    bola.setAttribute("class", "bola");
+    var p1 = Math.floor(Math.random() * 500);
+    var p2 = Math.floor(Math.random() * 400);
+    bola.setAttribute("style", "left:"+p1+"px;top:"+p2+"px;");
+    bola.setAttribute("onclick", "estourar(this)");
+
+    document.body.appendChild(bola);
+}
+
+function estourar(elemento){
+    document.body.removeChild(elemento);
+}
+
+function iniciar() {
+    setInterval(addBola, 1000);
+}
+*/
+
