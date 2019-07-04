a75f46192bc1c03c07d4866b86057f5e103eb67a
denis <dlcastro@artit.com.br>
2019-07-04 13:23:02 -0300

    Curso Finalizado

---
+++ jsAvancado/script.js
@@ -0,0 +1,31 @@
+/*
+function acao() {
+    document.write("Executou...<br>");
+}
+
+var timer = setInterval(acao, 2000);
+//clearInterval(timer); --pausa o "timer'
+
+//setTimeout(acao, 2000);
+//clearTimeout(timer); --pausa o "timer'
+*/
+
+function animal() {
+    this.raca = "Cão";
+    this.nome = "Lulu";
+    this.idade = "200";
+    this.peso = "10";
+}
+
+var lulu = new animal() 
+
+/*
+var cachorro ["raca"] = "husky";
+var cachorro ["nome"] = "apollo";
+var cachorro ["idade"] = 5;
+var cachorro ["peso"] = 25;
+
+
+
+document.write(cachorro);
+*/
